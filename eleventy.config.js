e6db57ae1b8a20fa6bb2ae9a6d9de19728f5177d
Jonesyu30 <jonesyu300@gmail.com>
2026-07-17 22:48:59 +0800

    added teh plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,17 @@
-module.exports = function(eleventyConfig) {
+import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+
+export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addWatchTarget("src/assets/");
+  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
   return {
     pathPrefix: "/Profolio-Website/", 
 
     dir: {
-      input: "src",          
-      output: "docs",       
-      includes: "_includes"  
+      input: "src",
+      output: "docs",
+      includes: "_includes"
     }
   };
 };
