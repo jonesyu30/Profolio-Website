e8508f37aa660ec2d52fda7c8ec257198dcb4a7c
Jonesyu30 <jonesyu300@gmail.com>
2026-07-17 22:38:16 +0800

    renamed to docs

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",          // Tells Eleventy to build ONLY what is inside /src
-      output: "_site",       // Compiles output files directly into the root /_site
+      output: "docs",       // Compiles output files directly into the root /_site
       includes: "_includes"  // Resolves paths relative to src/ (src/_includes)
     }
   };
